9ceaba98de2763764d703ff78e0d298d472680b9
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-03-19 14:59:17 -0500

    added frontend register functionality

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-
+import Revue from 'revue';
+import Vue from 'vue';
 import reducer from './reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
